997677fd18d3d52edfc7009f738a6ab5f68d34c0
Louvani <paula.louvani@gmail.com>
2022-04-01 15:45:47 -0500

    Sripts to initialize SDK and deploy contract for the new NFT

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,35 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+(async () => {
+  try {
+    const editionDropAddress = await sdk.deployer.deployEditionDrop({
+      // Collection name
+      name: "BaoDAO Membership",
+      // A description for a collection
+      description: "A DAO for restaurants owners",
+      // The image that will be held on our NFT!
+      image: readFileSync("scripts/assets/BAO.png"),
+      // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the contract.
+      // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
+      // you can set this to your own wallet address if you want to charge for the drop.
+      primary_sale_recipient: AddressZero,
+    });
+
+    // this initialization returns the address of our contract
+    // we use this to initialize the contract on the thirdweb sdk
+    const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+    // get metadata of the contract
+    const metadata = await editionDrop.metadata.get();
+
+    console.log(
+      "✅ Successfully deployed editionDrop contract, address:",
+      editionDropAddress,
+    );
+    console.log("✅ editionDrop metadata:", metadata);
+  } catch (error) {
+    console.log("failed to deploy editionDrop contract", error)
+  }
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  // hooks provieded for thirdweb
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address: ", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to Bao DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect to wallet
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!   </h1>
     </div>
   );
 };
