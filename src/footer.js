55ff1af30756f47d7cfd670904c7b45308d33df2
Louvani <paula.louvani@gmail.com>
2022-04-25 20:26:28 -0500

    create component for footer

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer-container'>
+        <div>BaoDao</div>
+        <div>Copyright © 2022 Bao DAO.</div>
+        <div>All Rights Reserved.</div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
